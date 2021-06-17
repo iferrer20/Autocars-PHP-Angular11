b4142d51dca9016760f2622f5b9095228c75f023
iferrer20 <iferreriestacio@gmail.com>
2021-06-17 18:51:04 +0200

    README

--- README.md
+++ README.md
@@ -11,3 +11,56 @@ Para iniciar los contenedores
 ```console
 docker-compose up
 ```
+
+## Caracteristicas principales
+- Home
+- Shop
+	- Filtros
+	- Favoritos
+	- Paginación
+- Login
+- Register
+- Recover password
+- Verify
+- Social login
+	- Gmail
+	- Github
+- Carrito
+	- Checkout
+- Mailjet
+
+## Mejoras
+- Angular 12 con Typescript
+	- Guards: Auth guard
+	- i18n (traducciones)
+	- Filtros en la url
+	- Popup component
+	- Stock, no poder comprar más
+	- Botones profesionales firebase social login
+	- EventBus service (rxjs)
+- PHP 8.0 framework propio por modulos
+	- Protección sql injection (database.php)
+	- Procedures MYSQL: 
+		- createCar: Crear coche
+		- deleteCar: Borrar coche
+		- userSignup: Registrar un nuevo usuario
+		- userSignin: Logearse 
+		- userChangePassword: Cambiar contraseña de un usuario
+		- userSocialSignin: Social signin
+		- searchCar: Buscar coches con filtros
+		- setFavoriteCar: Poner coche a favoritos
+		- unsetFavoriteCar: Quitar coche de favoritos
+		- getCart: Obtener carrito
+		- addToCart: Añadir al carrito
+		- delFromCart: Eliminar del carrito
+		- getCart: Obtener carrito de un usuario
+		- cartCheckout: Facturar carrito
+	- Tirar excepciones 45000 de mysql al cliente
+		- Utils, jsons, middlewares 
+	- Comprobación token firebase 
+	- Expiración JWT 
+	- Atributos y tipado de php 8.0
+	- App -> Call_action (transformar el body http y llamar al controlador con parametros)
+	- App -> Call_attributes (cargar librerias, midlewares, utils)
+- Servidor nginx con proxy a ng serve
+- Dockerizado: docker-compose.yml y Dockerfile
--- src/backend/modules/cars/controller.php
+++ src/backend/modules/cars/controller.php
@@ -4,28 +4,6 @@ use Utils\res;
 
 class CarsController extends Controller {
 
-    // public function list_get() {
-    //     res($this->model->get_cars());
-    // }
-    // public function list_post() {
-        
-    //     $list = new CarList();
-    //     array_to_obj(Client::$data, $list);
-    //     $list->validate();
-
-    //     $cars = $this->model->get_cars($list);
-
-    //     $car_count = $this->model->get_car_count();
-    //     $pages = $car_count/$list->limit;
-    //     $pages += is_float($pages) ? 1 : 0;
-    //     $pages = intval($pages);
-
-    //     res(array(
-    //         "cars" => $cars,
-    //         "pages" => $pages
-    //     ));
-        
-    // }
     public function categories_get() {
         //var_dump($this->model->get_categories());
         Utils\res($this->model->get_categories());
@@ -45,33 +23,6 @@ class CarsController extends Controller {
             'pages' => $pages
         ));
     }
-    public function create_post() {
-        $car = new Car();
-        Utils\array_to_obj($_POST, $car, true);
-
-        $car->validate();
-        $id = $this->model->insert_car($car);
-
-        Utils\save_image('img', 'public/img/cars/' . $id . '.jpg');
-    }
-    public function delete_post(int $id) {
-        $car = $this->model->get_car($id);
-        if ($car) {
-            $this->model->delete_car($car["id"]);
-            Utils\remove_file('/var/www/html/img/cars/' . $car["id"] . '.jpg');
-        } else {
-            Utils\notfound('The car ' . $id);
-        }
-    }
-    public function update_post(Car $car) {
-        $this->model->update_car($car);
-        if (Utils\is_image('img')) {
-            $filepath = 'public/img/cars/' . $car->id . '.jpg';
-            Utils\remove_file($filepath);
-            Utils\save_image('img', $filepath);
-        }
-    }
-
     #[middlewares('check_jwt')]
     public function favorite_put(string $car_id) {
         $this->model->set_favorite_car(Client::$jwt_data['user_id'], $car_id);
